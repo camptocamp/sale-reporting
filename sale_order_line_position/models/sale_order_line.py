992b823de071acf4ffe4279baebfc924fa1d049d
Matthieu Méquignon <matthieu.mequignon@camptocamp.com>
2021-06-23 12:17:54 +0200

    fixup! fixup! fixup! fixup! 13.0][ADD] sale_order_line_position

--- sale_order_line_position/models/sale_order_line.py
+++ sale_order_line_position/models/sale_order_line.py
@@ -31,20 +31,23 @@ class SaleOrderLine(models.Model):
             for line in vals_list
             if not line.get("display_type") and line.get("order_id")
         ]
-        ids = tuple(set(sale_ids))
-        self.flush()
-        query = """
-        SELECT order_id, max(position) FROM sale_order_line
-        WHERE order_id in %s GROUP BY order_id;
-        """
-        self.env.cr.execute(query, (ids,))
-        default_pos = {key: 1 for key in ids}
-        existing_pos = {order_id: pos + 1 for order_id, pos in self.env.cr.fetchall()}
-        sale_pos = {**default_pos, **existing_pos}
-        for line in vals_list:
-            if not line.get("display_type"):
-                line["position"] = sale_pos[line["order_id"]]
-                sale_pos[line["order_id"]] += 1
+        if sale_ids:
+            ids = tuple(set(sale_ids))
+            self.flush()
+            query = """
+            SELECT order_id, max(position) FROM sale_order_line
+            WHERE order_id in %s GROUP BY order_id;
+            """
+            self.env.cr.execute(query, (ids,))
+            default_pos = {key: 1 for key in ids}
+            existing_pos = {
+                order_id: pos + 1 for order_id, pos in self.env.cr.fetchall()
+            }
+            sale_pos = {**default_pos, **existing_pos}
+            for line in vals_list:
+                if not line.get("display_type"):
+                    line["position"] = sale_pos[line["order_id"]]
+                    sale_pos[line["order_id"]] += 1
         return super().create(vals_list)
 
     def unlink(self):
