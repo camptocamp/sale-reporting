3db71d910867bb9e86bc8046143723e48c4e4083
Ivàn Todorovich <ivan.todorovich@gmail.com>
2022-05-18 16:34:06 -0300

    [ADD] sale_order_invoice_date

---
+++ sale_order_invoice_date/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Camptocamp SA (https://www.camptocamp.com).
+# @author Iván Todorovich <ivan.todorovich@camptocamp.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Order Invoice Date",
+    "summary": "Display the invoice date on Sales Order analysis reports",
+    "version": "14.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainers": ["ivantodorovich"],
+    "website": "https://github.com/OCA/sale-reporting",
+    "license": "AGPL-3",
+    "category": "Sales",
+    "depends": ["sale"],
+}
