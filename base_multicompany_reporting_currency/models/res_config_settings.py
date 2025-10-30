c0a86e79cdd5ac15e381eb4b913186a482e2a0df
Iván Todorovich <ivan.todorovich@gmail.com>
2025-10-30 09:40:37 -0300

    [REF] sale_multicompany_reporting_currency: move some utility methods to base module

---
+++ base_multicompany_reporting_currency/models/res_config_settings.py
@@ -0,0 +1,23 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    multicompany_reporting_currency = fields.Many2one(
+        "res.currency",
+        config_parameter="base_multicompany_reporting_currency.multicompany_reporting_currency",
+    )
+
+    def set_values(self):
+        applied_currency = self.env[
+            "res.company"
+        ]._get_multicompany_reporting_currency()
+        super().set_values()
+        to_apply_currency = self.multicompany_reporting_currency
+        if applied_currency != to_apply_currency:
+            self.env["res.company"]._recompute_multicompany_reporting_currency()
+        return True
